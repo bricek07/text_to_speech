009fd5055bb8dade2dc321f1808a9bbc9cc975a9
Brice Tegankong <brice.tegankongkono@telecom-paris.fr>
2024-07-18 17:56:21 +0200

    creds modification

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from google.oauth2 import service_account
 @st.cache_data
 def text_to_speech(text):
     # Instantiates a client
-    creds = service_account.Credentials.from_service_account_file(st.secrets.google_creds)
+    creds = service_account.Credentials.from_service_account_info(st.secrets.google_creds)
     client = texttospeech.TextToSpeechClient(credentials=creds)
 
     # Set the text input to be synthesized
